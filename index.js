03c17facae5f42fbbc62d050a1a793fe02fca680
Matthew Luvin <jmluvin@gmail.com>
2019-01-23 01:41:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,6 +15,6 @@ function deleteFromObjectByKey(object, key) {
   return newObject
 }
 function destructivelyDeleteFromObjectByKey(object, key) {
-  delete Object[key]
-  return Object
+  delete object[key]
+  return object 
 }
